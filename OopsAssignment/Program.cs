557d92fc92b13242bbe07c43ef7cf6b680c312cf
phoenix180 <tusharwangade@gmail.com>
2022-03-21 11:56:02 +0530

    Initial committ of the project

---
+++ OopsAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OopsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            if (a > 4)
+            { 
+            Console.WriteLine("Hello World!");
+        }
+        }
+    }
+}
