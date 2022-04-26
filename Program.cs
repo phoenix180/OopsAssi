5229c45938f780dd13dae66a61b680f0d3b1dc5a
Tushar Wangade <102008407+phoenix180@users.noreply.github.com>
2022-04-26 00:33:23 -1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    internal class SumofDigit
+    {
+        int getSum(int n)
+        {
+            //declare variable sum for calculating sum
+            int sum;
+
+            /* Single line that calculates sum */
+            for (sum = 0; n > 0; sum += n % 10, n /= 10)
+                ;
+
+            return sum;
+        }
+
+        // Entry Point
+        public static void Main()
+        {
+            //try catch block until suitable input is provided
+            Again:
+            try
+            {
+
+                Console.Write("Input  a number(integer): ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                SumofDigit sumdigit = new SumofDigit();
+                //Suitable output
+                Console.Write(sumdigit.getSum(n));
+            }
+            catch
+            {
+                goto Again;
+            }
+        }
+    }
+}
